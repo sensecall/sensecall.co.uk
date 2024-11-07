cb156df9f0d29c39c3e3f90671e0d5245fcf7b48
Dan Sensecall <dan@sensecall.co.uk>
2024-11-07 12:11:18 +0000

    content

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,36 @@ module.exports = {
       'serif': ['"Crimson Pro"', 'serif'],
       'sans': ['"Work Sans"', 'sans-serif'],
     },
+    typography: {
+      DEFAULT: {
+        css: {
+          color: '#384042',
+          a: {
+            color: '#e11d48',
+            '&:hover': {
+              color: '#be123c',
+            },
+          },
+          'h1,h2,h3,h4': {
+            fontFamily: '"Crimson Pro", serif',
+            fontWeight: '700',
+          },
+        },
+      },
+      dark: {
+        css: {
+          color: '#e0e2e3',
+          a: {
+            color: '#ff5773',
+            '&:hover': {
+              color: '#ff8599', 
+            },
+          },
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
 }
